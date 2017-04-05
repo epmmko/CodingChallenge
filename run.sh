581019eaf211091257aa9039824c5694f79fd5cf
epmmko <epmmko@gmail.com>
2017-04-04 23:04:19 -0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,1 +1,13 @@
-#!/usr/bin/env bash
+#!/bin/env bash
+g++ -std=c++11 ./src/Feature1.cpp -o ./log_input/Feature1.exe
+g++ -std=c++11 ./src/Feature2.cpp -o ./log_input/Feature2.exe
+g++ -std=c++11 ./src/Feature3.cpp -o ./log_input/Feature3.exe
+g++ -std=c++11 ./src/Feature4.cpp -o ./log_input/Feature4.exe
+./log_input/Feature1.exe
+./log_input/Feature2.exe
+./log_input/Feature3.exe
+./log_input/Feature4.exe
+mv ./log_input/blocked.txt ./log_output/blocked.txt
+mv ./log_input/hosts.txt ./log_output/hosts.txt
+mv ./log_input/resources.txt ./log_output/resources.txt
+mv ./log_input/hours.txt ./log_output/hours.txt
